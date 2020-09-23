e00068f6032967cefad5a964f999ba2dd26bffb1
Peterson <pepe419@gmail.com>
2020-09-23 19:57:19 -0300

    main vazia

---
+++ src/br/com/digitalhouse/exerciciointegrador/Livro.kt
@@ -0,0 +1,5 @@
+package br.com.digitalhouse.exerciciointegrador
+
+class Livro(codigo: Int, titulo: String, autor: String, anoLancamento: Int, qtdEsoque: Int, preco:Double) {
+    
+}
--- src/br/com/digitalhouse/exerciciointegrador/Main.kt
+++ src/br/com/digitalhouse/exerciciointegrador/Main.kt
@@ -1,5 +1,5 @@
 package br.com.digitalhouse.exerciciointegrador
 
 fun main(){
-    
+
 }
